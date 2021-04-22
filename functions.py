a5d0a9a178fd3fcc8b0d4192ae8dd0060a73d3d8
unknown <f.f.bondar@gmail..com>
2021-04-22 17:51:02 +0300

    initial commit

---
+++ functions.py
@@ -0,0 +1,8 @@
+FUNCTIONS = {
+    'a': lambda x: x ** 2,
+    'b': lambda x: 2 * x + 1,
+    'c': lambda x: x - 1,
+    'd': lambda x: x / 10,
+    'e': lambda x: x + 10,
+    'f': lambda x: x / 2,
+}
